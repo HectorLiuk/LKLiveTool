2ed5a7db9191fb0fbed958841326d3c2a258cedb
snail_lk <544523660@qq.com>
2016-09-06 10:21:48 +0800

    企业账号发布

--- LKLiveTool/LKLiveTool/Root/MainTabBarController.swift
+++ LKLiveTool/LKLiveTool/Root/MainTabBarController.swift
@@ -17,24 +17,7 @@ class MainTabBarController: RAMAnimatedTabBarController {
     
     //设置访问直播权限
     private func setUpAccessAuthority() -> Bool{
-        if UIDevice.deviceVersion() == "iPhone Simulator" {
-            log.error("不能使用模拟器运行")
-            return false
-        }
-        
-        if !UIImagePickerController.isSourceTypeAvailable(.Camera) {
-           log.error("您的设备没有摄像头或者相关的驱动, 不能进行直播")
-            return false
-        }
-        
-        //获取照相机访问权限
-        let authStatus:AVAuthorizationStatus = AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
-        if(authStatus == AVAuthorizationStatus.Denied || authStatus == AVAuthorizationStatus.Restricted) {
-            
-            return false
-        }else{
-            return true
-        }
+       
         
         return true
         
@@ -45,14 +28,28 @@ class MainTabBarController: RAMAnimatedTabBarController {
 extension MainTabBarController : UITabBarControllerDelegate{
     func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool{
         if viewController.isMemberOfClass(UINavigationController) {
-            setUpAccessAuthority()
+            if UIDevice.deviceVersion() == "iPhone Simulator" {
+                log.error("不能使用模拟器运行")
+                return false
+            }
             
-//            let sb = UIStoryboard(name: "Living", bundle: nil)
-//            let livingVC = sb.instantiateInitialViewController()
-//            
-//            presentViewController(livingVC!, animated: true, completion: nil)
+            if !UIImagePickerController.isSourceTypeAvailable(.Camera) {
+                log.error("您的设备没有摄像头或者相关的驱动, 不能进行直播")
+                return false
+            }
             
+            //获取照相机访问权限
+            let authStatus:AVAuthorizationStatus = AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
+            if(authStatus == AVAuthorizationStatus.Denied || authStatus == AVAuthorizationStatus.Restricted) {
+                log.error("没有授权相机")
+                return false
+            }
+            
+            let sb = UIStoryboard(name: "Living", bundle: nil)
+            let livingVC = sb.instantiateInitialViewController()
+            presentViewController(livingVC!, animated: true, completion: nil)
             return false
+            
         }
 
         
