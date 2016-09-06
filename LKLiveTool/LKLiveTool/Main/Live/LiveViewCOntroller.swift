1642442237f73caa541426ddc846bb854e177445
snail_lk <544523660@qq.com>
2016-09-06 14:38:33 +0800

    直播上传推流

--- LKLiveTool/LKLiveTool/Main/Live/LiveViewCOntroller.swift
+++ LKLiveTool/LKLiveTool/Main/Live/LiveViewCOntroller.swift
@@ -6,13 +6,126 @@
 //  Copyright © 2016年 LK. All rights reserved.
 //
 
-import GPUImage
 
 class LiveViewCOntroller: BasicViewController {
     
+    
+    @IBOutlet weak var livingStateLabel: UILabel!
+    
+    @IBOutlet weak var beautyButton: UIButton!
+    
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    @IBOutlet weak var closeButton: UIButton!
+    
+    
+    
+    lazy var session: LFLiveSession = {
+        /// 音频设置
+        let audioConfiguration = LFLiveAudioConfiguration.defaultConfiguration()
+        /// 视频设置  
+        let videoConfiguration = LFLiveVideoConfiguration.defaultConfigurationForQuality(.Low3, outputImageOrientation: .Portrait)
+        let session = LFLiveSession(audioConfiguration: audioConfiguration, videoConfiguration: videoConfiguration)
+        
+        session?.delegate = self
+        session?.preView = self.view
+
+        return session!
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.whiteColor()
+        view.backgroundColor = UIColor.clearColor()
+        requestAccessForVideo()
+        requestAccessForAudio()
+        
+    }
+    //MARK: -------------
+    //MARK: 页面按钮触发事件
+    //美颜
+    @IBAction func didTappedBeautyButton(sender: UIButton) {
+        session.beautyFace = !session.beautyFace
+        sender.selected = !session.beautyFace
+    }
+    
+    //摄像头 前置后置
+    @IBAction func didTappedCameraButton(sender: AnyObject) {
+        let devicePositon = session.captureDevicePosition
+        
+        session.captureDevicePosition = (devicePositon == AVCaptureDevicePosition.Back) ? AVCaptureDevicePosition.Front : AVCaptureDevicePosition.Back;
+    }
+    
+    //关闭
+    @IBAction func didTappedCloseButton(sender: UIButton) {
+        dismissViewControllerAnimated(true) { 
+            
+        }
+    }
+    
+    //开启直播  结束直播
+    @IBAction func didTappedStartLiving(sender: UIButton) {
+        sender.selected = !sender.selected;
+        if (sender.selected) {
+            let stream = LFLiveStreamInfo()
+            stream.url = "rtmp://30.96.179.95:1935/live/1234"
+            session.startLive(stream)
+        } else {
+            session.stopLive()
+        }
+    }
+    
+    
+    
+    
+    //MARK: 视频权限AccessAuth
+    func requestAccessForVideo() -> Void {
+        let status = AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
+        switch status  {
+        // 许可对话没有出现，发起授权许可
+        case AVAuthorizationStatus.NotDetermined:
+            AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo, completionHandler: { (granted) in
+                if (granted) {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.session.running = true;
+                    });
+                }
+            })
+            break;
+        // 已经开启授权，可继续
+        case AVAuthorizationStatus.Authorized:
+            session.running = true;
+            break;
+        // 用户明确地拒绝授权，或者相机设备无法访问
+        case AVAuthorizationStatus.Denied: break
+        case AVAuthorizationStatus.Restricted:break;
+        
+        }
+    }
+    
+    //MARK: 音频权限
+    func requestAccessForAudio() -> Void {
+        let status = AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeAudio)
+        switch status  {
+        // 许可对话没有出现，发起授权许可
+        case AVAuthorizationStatus.NotDetermined:
+            AVCaptureDevice.requestAccessForMediaType(AVMediaTypeAudio, completionHandler: { (granted) in
+            })
+            break;
+        // 已经开启授权，可继续
+        case AVAuthorizationStatus.Authorized:
+            break;
+        // 用户明确地拒绝授权，或者相机设备无法访问
+        case AVAuthorizationStatus.Denied: break
+        case AVAuthorizationStatus.Restricted:break;
+        
+        }
     }
 
 }
+
+extension LiveViewCOntroller: LFLiveSessionDelegate{
+    
+}
+
--- LKLiveTool/LKLiveTool/Root/MainTabBarController.swift
+++ LKLiveTool/LKLiveTool/Root/MainTabBarController.swift
@@ -15,35 +15,13 @@ class MainTabBarController: RAMAnimatedTabBarController {
         self.delegate = self
     }
     
-    //设置访问直播权限
-    private func setUpAccessAuthority() -> Bool{
-       
-        
-        return true
-        
-    }
+    
     
 }
 
 extension MainTabBarController : UITabBarControllerDelegate{
     func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool{
         if viewController.isMemberOfClass(UINavigationController) {
-            if UIDevice.deviceVersion() == "iPhone Simulator" {
-                log.error("不能使用模拟器运行")
-                return false
-            }
-            
-            if !UIImagePickerController.isSourceTypeAvailable(.Camera) {
-                log.error("您的设备没有摄像头或者相关的驱动, 不能进行直播")
-                return false
-            }
-            
-            //获取照相机访问权限
-            let authStatus:AVAuthorizationStatus = AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
-            if(authStatus == AVAuthorizationStatus.Denied || authStatus == AVAuthorizationStatus.Restricted) {
-                log.error("没有授权相机")
-                return false
-            }
             
             let sb = UIStoryboard(name: "Living", bundle: nil)
             let livingVC = sb.instantiateInitialViewController()
